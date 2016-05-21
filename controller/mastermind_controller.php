70761061bd2e95d748787c7cb268c8b54dd25c3a
Renato Francisco Wesenauer <renato.wesenauer@webmotors.local>
2016-05-20 21:34:07 -0300

    first commit vanhackathon

---
+++ controller/mastermind_controller.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * Class tha contains the start of actions 
+	 * @author Renato Wesenauer <renato.wesenauer@gmail.com>
+	 * @since 2016-05-20
+	 * @access public
+	 */
+	class mastermind_controller
+	{
+		/** 
+		 * Start a new game
+		 * @access public
+		 */
+		public function new_game()
+		{
+
+		}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	set_time_limit(0);
+	date_default_timezone_set("America/Sao_Paulo");
+
+	$method = $_GET["method"];
+
+	require_once("controller/mastermind_controller.php");
+	require_once("model/mastermind.php");
+
+	$mastermind = new mastermind_controller();
+
+	if (method_exists($mastermind, $method))
+		$mastermind->$method();
+	else
+		echo("Method does not exist");
+?>
